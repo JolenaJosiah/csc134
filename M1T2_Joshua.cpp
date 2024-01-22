ab0d6a7471e7d7ed21e7835f9168028c3662bc66
JolenaJosiah <157392532+JolenaJosiah@users.noreply.github.com>
2024-01-22 16:21:22 +0000

    M1T2

---
+++ M1T2_Joshua.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M1T2
+// Jolena
+// 1/22/24
+
+ #include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello World!" << endl;
+    cout << "my name is Jolena JosiahJoshua" << "\n";
+    cout << "I love learning languages and I am facinaed now in the languages of computers" << endl;
+
+    // Ask a question
+    string name;
+    cout << "what's your name? ";
+    cin >> name;
+
+    cout << "Nice to meet you" << name << endl;
+
+    cout << endl << endl;
+    return 0;
+}
+
