25554e29a3f6b24d7e2e12d1206e4d601e65edde
JolenaJosiah <157392532+JolenaJosiah@users.noreply.github.com>
2024-01-29 16:16:06 +0000

    M2T1 - recipt with wrong total

---
+++ .vscode/M2T1_Joshua.cpp
@@ -0,0 +1,32 @@
+/*
+// CSC 134
+// M2T1 - Receipt
+// Jolena
+// 1/29/24
+*/
+
+ #include <iostream>
+ #include <string>
+ using namespace std;
+
+ int main()
+ {
+    cout << "M2T1" << endl;
+    //set up variables
+    string meal = "value Meal";
+    // all in $, except the percent;
+    double meal_price = 5.99;
+    double tax_percent = 0.08;
+    double tax_amount = 0;
+    double total = 0;
+
+    // do calculations
+
+    // print the recipt
+    // TODO: make it line up, and fix decimals
+    cout << "meal" << "\t$" << meal_price << endl;
+    cout << "tax" << "\t\t$" << tax_amount << endl;
+    cout << "total" << "\t\t$" << total << endl;
+
+    return 0;
+ }
