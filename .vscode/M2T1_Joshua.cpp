34446300b27d70ab7d0eec44c36498a69405873a
JolenaJosiah <157392532+JolenaJosiah@users.noreply.github.com>
2024-01-29 16:41:05 +0000

    Finished M2T1

--- .vscode/M2T1_Joshua.cpp
+++ .vscode/M2T1_Joshua.cpp
@@ -7,11 +7,13 @@
 
  #include <iostream>
  #include <string>
+ #include <iomanip>
  using namespace std;
 
  int main()
  {
     cout << "M2T1" << endl;
+    cout << "Thank you for your service" << endl;
     //set up variables
     string meal = "value Meal";
     // all in $, except the percent;
@@ -21,10 +23,17 @@
     double total = 0;
 
     // do calculations
+   tax_amount = meal_price * tax_percent;
+   total = meal_price + tax_amount;
 
-    // print the recipt
-    // TODO: make it line up, and fix decimals
-    cout << "meal" << "\t$" << meal_price << endl;
+    
+   // print the recipt
+   // print this one to set the decimals to exactly 2
+   // at he top add:
+    // #include <iomanip>
+    cout << fixed << setprecision (2);
+
+    cout << "meal" << "\t\t$" << meal_price << endl;
     cout << "tax" << "\t\t$" << tax_amount << endl;
     cout << "total" << "\t\t$" << total << endl;
 
