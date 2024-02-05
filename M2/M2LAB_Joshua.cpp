1524c73d22f58c86137f146e15588e6c00bb16aa
JolenaJosiah <157392532+JolenaJosiah@users.noreply.github.com>
2024-02-05 16:41:44 +0000

    FINISHED_M2LAB

--- M2/M2LAB_Joshua.cpp
+++ M2/M2LAB_Joshua.cpp
@@ -14,6 +14,9 @@
     double volume;
     const double COST_PER_CUBIC_FOOT = 0.23;
     const double CHARGE_PER_CUBIC_FOOT = 0.5;
+    double cost, charge, profit;
+
+
 
     // Ask for user input
    cout << "lengh? ";
@@ -27,10 +30,13 @@
 
     // Do calculations
     volume = length * width * height;
-
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost;
     // print the answer
     cout << "The volume is: " << volume << endl;
-
-
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
     return 0;
  }
